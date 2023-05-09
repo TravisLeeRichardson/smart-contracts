3fb2110de9f41cf4c61ff35b37d630ec4dbe6644
travisleerichardson <tlrichar@gmail.com>
2023-05-09 09:51:12 -0400

    chainlink spring hackathon defi apps

---
+++ .deps/npm/contracts/Token.sol
@@ -0,0 +1,26 @@
+//Begin
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract Token is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor() ERC20("Chainlink Hack Token", "CHT") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return 2;
+    }    
+}
+
+//End
